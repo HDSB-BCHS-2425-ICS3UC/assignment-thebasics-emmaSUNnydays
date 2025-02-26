0c4eb421fca21153192021a1f599fae6f23d9e33
emmaSUNnydays <1sunemm@hdsb.ca>
2025-02-26 13:28:08 -0500

    Improved UX
    added pauses

--- typeWork.py
+++ typeWork.py
@@ -2,6 +2,8 @@
 # Date Modified: 2025/02/24
 # Description: Describe different data types.
 
+import time
+
 # these are examples of data types.
 integer = 15
 float = 4.5
@@ -17,8 +19,14 @@ booleanDefinition = "true or false"
 stringDefinition = "a combination of characters"
 
 # these are print statements that define and provide an example for each data type.
+print("Here are some data types and their definitions:")
+time.sleep(1.5)
 print(f"An integer is {integerDefinition} and an example is {integer}.")
+time.sleep(1.5)
 print(f"A float is a {floatDefinition} and an example is {float}.")
+time.sleep(1.5)
 print(f"A character/char is a {charDefinition} and an example is {char}.")
+time.sleep(1.5)
 print(f"A boolean is {booleanDefinition} and an example is {boolean}.")
+time.sleep(1.5)
 print(f"A string is {stringDefinition} and an example is {string}.")
