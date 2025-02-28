648ec9d1424c7f75504a3ad0640992f02f438d03
emmaSUNnydays <1sunemm@hdsb.ca>
2025-02-28 14:00:22 -0500

    Added comments

--- 3DVolume.py
+++ 3DVolume.py
@@ -1,10 +1,3 @@
-"""
-3D Volume
-Write a program that calculates the volume o of the three-dimensional shapes mentioned to the right. It should take as input the parameter(s) necessary to calculate the volume (A, r , h ). You should take in each variable you need in turn. Ie. for the cube, take in the length, for the sphere, take in the radius, etc…
-
-The output should be USER FRIENDLY.
-"""
-
 # Author: Emma Sun
 # Date modified: 2025/02/27
 # Description: Calculates volume of a 3D shape with the values that the user provides.
@@ -27,28 +20,28 @@ shape = shape.lower()
 if "cube" in shape:
       print("Great, we're going to calculate the volume of a cube!")
       time.sleep(1.5)
-      l = float(input("What is the side length of your cube?\n"))
-      v = l**3
-      print(f"The volume of your cube is {v}!")
+      l = float(input("What is the side length of your cube?\n")) # user inputs side length
+      v = l**3 # calculates volume of cube
+      print(f"The volume of your cube is {v}!") # prints volume of sphere
 elif "sphere" in shape:
       print("Great, we're going to calculate the volume of a sphere!")
       time.sleep(1.5)
-      r = float(input("What is the radius of your sphere?\n"))
-      v = round(4/3*math.pi*r**3)
-      print(f"The volume of your sphere is ~{v}!")
+      r = float(input("What is the radius of your sphere?\n")) # user inputs radius
+      v = round(4/3*math.pi*r**3) # calculates volume of sphere
+      print(f"The volume of your sphere is ~{v}!") # prints volume of sphere
 elif "cone" in shape:
       print("Great, we're going to calculate the volume of a cone!")
       time.sleep(1.5)
-      r = float(input("What is the radius of the base of your cone?\n"))
-      h = float(input("What is the height of your cone?\n"))
-      v = round(1/3*math.pi*r**2*h)
-      print(f"The volume of your cone is ~{v}!")
+      r = float(input("What is the radius of the base of your cone?\n")) # user inputs radius
+      h = float(input("What is the height of your cone?\n")) # user inputs height
+      v = round(1/3*math.pi*r**2*h) # calculates volume of cone
+      print(f"The volume of your cone is ~{v}!") # prints volume of cone
 elif "cylinder" in shape:
       print("Great, we're going to calculate the volume of a cylinder!")
       time.sleep(1.5)
-      r = float(input("What is the radius of the base of your cylinder?\n"))
-      h = float(input("What is the height of your cylinder?\n"))
-      v = round(math.pi*r**2*h)
-      print(f"The volume of your cone is ~{v}!")
-else:
+      r = float(input("What is the radius of the base of your cylinder?\n")) # user inputs radius
+      h = float(input("What is the height of your cylinder?\n")) # user inputs height
+      v = round(math.pi*r**2*h) # calculates volume of cylinder
+      print(f"The volume of your cone is ~{v}!") # prints volume of cylinder
+else: # print if user enters something that isn't one of the given shapes
       print("I don't know that shape yet!")
